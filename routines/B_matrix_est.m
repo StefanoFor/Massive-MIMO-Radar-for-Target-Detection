4dff8560925acfd994de935995617d811e0d18e9
Stefano Fortunati <stefano.fortunati@centralesupelec.fr>
2021-05-12 15:47:37 +0200

    Update B_matrix_est.m

--- routines/B_matrix_est.m
+++ routines/B_matrix_est.m
@@ -8,8 +8,6 @@ B_2 = 0;
 for m = 1:l
     for n = m+1 : N
         B_2 = B_2 + 2*real(hat_c(n)*conj(v(n))* conj(hat_c(n-m))*v(n-m)); 
-        %B_3 = B_3 + 2*real(conj(hat_c(n))*v(n)* hat_c(n-m)*conj(v(n-m))); 
-        %B_2 = B_2 + 2*real(hat_c(n)*v(n)* conj(hat_c(n-m))*conj(v(n-m))); 
     end
 end
 
